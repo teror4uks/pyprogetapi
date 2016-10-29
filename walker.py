bd1e5f42d48436a2ce2347091b6de4d68b91448d
Pavel Sirotkin <pav.pnz@gmail.com>
2016-10-29 15:59:51 +0300

    add func buget package list

--- walker.py
+++ walker.py
@@ -28,10 +28,27 @@ class Walker:
             print(result['Feed_Id'])
         return result['Feed_Id']
 
+    def get_nuget_packages_list(self):
+        reader = codecs.getreader('utf-8')
+        _feed_id = self.getfeedid()
+        parametrs = {"API_Key": self.api_key, "Feed_Id": _feed_id}
+        http = urllib3.PoolManager()
+        response = http.request(method='GET', url=self.url \
+                                                  + '/api/json/NuGetPackages_GetPackages?' \
+                                                  + "API_Key=" \
+                                                  + parametrs["API_Key"] \
+                                                  + '&' + 'Feed_Id=' \
+                                                  + str(parametrs["Feed_Id"]), preload_content=False)
+        result = json.load(reader(response))
+
+        if Walker.DEBUG == 1:
+            print(result)
+        return result
+
     def __str__(self):
         return "API = {0}, FEED = {1}, URL = {2}".format(self.api_key, self.feed, self.url)
 
 if __name__ == '__main__':
     walker = Walker(url='http://172.30.114.63:81', feed='Default', api_key='WRsdqWhltrOLePrsgWxcvZasTGpfgG', DEBUG=1)
     # walker.getfeedid()
-    walker.getfeedid()
+    walker.get_nuget_packages_list()
