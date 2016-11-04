e28fad089f42b1950dd2dc465752a62ce448f25b
Pavel Sirotkin <pav.pnz@gmail.com>
2016-11-04 16:08:21 +0300

    add func get_format_nuget_pack_list()

--- walker.py
+++ walker.py
@@ -25,7 +25,7 @@ class Walker:
         result = json.load(reader(response))
 
         if Walker.DEBUG == 1:
-            print(result['Feed_Id'])
+            print('Feed_Id: ',result['Feed_Id'])
         return result['Feed_Id']
 
     def get_nuget_packages_list(self):
@@ -44,17 +44,21 @@ class Walker:
         if Walker.DEBUG == 1:
             #print(result[1]['Package_Id']) ['Version_Text']['Published_Date']
             print(result[0])
-            ar = []
-            for res in result:
-                temp = {}
-                for pack_id in res.keys():
-                    if pack_id == 'Package_Id':
-                        temp[pack_id] = res[pack_id]
-                        ar.append(temp)
-                    if pack_id == 'Version_Text':
-                        temp[pack_id] = res[pack_id]
-            print(ar)
+            res = self.get_format_nuget_pack_list(result)
+            print(res)
         return result
+
+    def get_format_nuget_pack_list(self, json_list):
+        ar = []
+        for res in json_list:
+            temp = {}
+            for pack_id in res.keys():
+                if pack_id == 'Package_Id':
+                    temp[pack_id] = res[pack_id]
+                    ar.append(temp)
+                if pack_id == 'Version_Text':
+                    temp[pack_id] = res[pack_id]
+        return ar
 
     def __str__(self):
         return "API = {0}, FEED = {1}, URL = {2}".format(self.api_key, self.feed, self.url)
